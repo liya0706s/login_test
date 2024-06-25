c778bef1065345f50034f0aabad8023cfb0a6af3
angielee <liya0706s@gmail.com>
2024-06-25 09:23:39 +0800

    login html and chk acc and chk pw api

---
+++ api/chk_acc.php
@@ -0,0 +1,18 @@
+<?php
+include_once "db.php";
+
+//利用count來檢查是否帳號已存在
+$res=$User->count(['acc'=>$_POST['acc']]);
+//回傳檢查的結果
+if($res>0){
+    echo 1;
+}else{
+    echo 0;
+}
+
+//簡化的寫法
+// echo $User->count(['acc'=>$_POST['acc']]);
+
+
+
+?>
