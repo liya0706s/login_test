0758c85efcc56d4cc6c79569516a0552a8bf8d71
angielee <liya0706s@gmail.com>
2024-06-25 09:12:36 +0800

    git init

---
+++ api/db.php
@@ -0,0 +1,99 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+class DB
+{
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db09";
+    protected $pdo;
+    protected $table;
+
+    public function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+
+    function q($sql)
+    {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function a2s($array)
+    {
+        foreach ($array as $col => $value) {
+            $tmp[] = "`$col`='$value'";
+        }
+        return $tmp;
+    }
+
+    function sql_all($sql, $array, $other)
+    {
+        if (isset($this->table) && !empty($this->table)) {
+            if (is_array($array)) {
+                if (!empty($array)) {
+                    $tmp = $this->a2s($array);
+                    $sql .= " where " . join(" && ", $tmp);
+                }
+            } else {
+                $sql .= " $array";
+            }
+            $sql .= $other;
+            return $sql;
+        }
+    }
+
+    function all($where = '', $other = '')
+    {
+        $sql = "select * from `$this->table`";
+        $sql = $this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function count($where = '', $other = '')
+    {
+        $sql = "select count(*) from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+   
+    function find($id)
+    {
+        $sql = "select * from `$this->table` ";
+
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " where " . join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " where `id`='$id'";
+        }
+        //echo 'find=>'.$sql;
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+
+    public function getUser($username)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE acc = :username");
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+}
+
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function to($url)
+{
+    header("location:$url");
+}
+
+
+// 每一張資料表的物件變數
+$Mem = new DB('mem');
